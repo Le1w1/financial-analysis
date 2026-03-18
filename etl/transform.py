b4eb13e5f96930f22c6d4359b229fbe6a4059b84
Leonel Jesus Rodriguez <LeonelJesus.Rodriguez@alumnos.uai.edu.ar>
2026-03-18 13:04:28 -0300

    feat: pipeline multi-accion AAPL MSFT GOOGL AMZN, CSVs en data/

--- etl/transform.py
+++ etl/transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import time
 
 def get_engine():
     return create_engine(
@@ -15,22 +16,40 @@ def calculate_metrics(df: pd.DataFrame) -> pd.DataFrame:
     df["daily_return"] = df["close"].pct_change()
     return df
 
-def load_to_sql(df : pd.DataFrame, table_name: str, engine)-> None:
+
+def load_to_sql(df: pd.DataFrame, table_name: str, engine, if_exists: str = "replace") -> None:
     df.to_sql(
-        name= table_name,
-        con= engine,
-        if_exists="replace",
-        index= True,
+        name=table_name,
+        con=engine,
+        if_exists=if_exists,
+        index=True,
         index_label="date"
-    )
+        )
     print(f"Carga exitosa: {table_name}")
+
+
 
 if __name__ == "__main__":
     from extract import get_stock_data
+    import os
 
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     engine = get_engine()
-    df_raw = get_stock_data("AAPL")
-    load_to_sql(df_raw, "stock_prices",engine)
+    symbols = ["AAPL", "MSFT", "GOOGL", "AMZN"]
 
-    df_metrics = calculate_metrics(df_raw.copy())
-    load_to_sql(df_metrics,"stock_metrics",engine)
+    for i, symbol in enumerate(symbols):
+        print(f"Procesando {symbol}...")
+
+        if_exists = "replace" if i == 0 else "append"
+
+        df_raw = get_stock_data(symbol)
+        load_to_sql(df_raw, "stock_prices", engine, if_exists)
+
+        df_metrics = calculate_metrics(df_raw.copy())
+        load_to_sql(df_metrics, "stock_metrics", engine, if_exists)
+
+        df_raw.to_csv(os.path.join(BASE_DIR, f"data/{symbol}_prices.csv"))
+        df_metrics.to_csv(os.path.join(BASE_DIR, f"data/{symbol}_metrics.csv"))
+
+        print(f"{symbol} completado.")
+        time.sleep(15)
